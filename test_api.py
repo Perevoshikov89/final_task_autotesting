ea602ec65f8ccda4ab996baf452a686f44dd5eff
Perevoshikov_89 <yuri.perevoshikov@gmail.com>
2024-06-13 19:53:22 +0400

    внесены исправления

--- test_api.py
+++ test_api.py
@@ -2,18 +2,27 @@ import requests
 import allure
 import pytest
 
-BASE_URL = "https://www.chitai-gorod.ru/"
+# BASE_URL = "https://www.chitai-gorod.ru/"
+# BASE_URL = 
+BASE_URL_2 = "https://web-gate.chitai-gorod.ru/api/v2/"
+Bearer = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MTgzODA1NTAsImlhdCI6MTcxODIxMjU1MCwiaXNzIjoiL2FwaS92MS9hdXRoL2Fub255bW91cyIsInN1YiI6Ijg1NmEyNjQ0NjZiYjA2Y2RlOGNiMzc4ZWNiNjc4MTM1ZmY2Yzk0MmE1NDI0Yjg0MzM1YmQxY2U4YjY1Y2I1ZTIiLCJ0eXBlIjoxMH0.UP79_uyJ3yb7nOPKIeIz3CWf58KBez1knlrqleWekM0"
 
 @allure.feature("API")
 @allure.story("Получение списка книг")
+@pytest.mark.api_test
 @pytest.mark.positive_test
 def test_get_books():
-    response = requests.get(f"{BASE_URL}/products")
+    response = requests.get("{BASE_URL_2}products" headers = headers)
+    headers = {
+          'content-type' : 'application/json',
+          'authorization' : 'Bearer'
+            }
     assert response.status_code == 200
 
 
 @allure.feature("API")
 @allure.story("Получение информации о книге по ID")
+@pytest.mark.api_test
 @pytest.mark.positive_test
 def test_get_book_by_id():
     book_id = 3018590
@@ -22,6 +31,7 @@ def test_get_book_by_id():
 
 @allure.feature("API")
 @allure.story("Поиск книг")
+@pytest.mark.api_test
 @pytest.mark.positive_test
 def test_search_books_rus():
     search_query = "Мастер и Маргарита"
@@ -30,6 +40,7 @@ def test_search_books_rus():
 
 @allure.feature("API")
 @allure.story("Поиск книг")
+@pytest.mark.api_test
 @pytest.mark.positive_test
 def test_search_books_eng():
     search_query = "Python"
@@ -38,6 +49,7 @@ def test_search_books_eng():
 
 @allure.feature("API")
 @allure.story("Пустое поле")
+@pytest.mark.api_test
 @pytest.mark.negative_test
 def test_search_empty():
     search_query = ""
@@ -47,6 +59,7 @@ def test_search_empty():
 
 @allure.feature("API")
 @allure.story("Пробел")
+@pytest.mark.api_test
 @pytest.mark.negative_test
 def test_search_space():
     search_query = "   "
@@ -55,6 +68,7 @@ def test_search_space():
 
 @allure.feature("API")
 @allure.story("Получение списка книг по категории")
+@pytest.mark.api_test
 @pytest.mark.positive_test
 def test_get_category_books():
         my_params = {
@@ -67,6 +81,7 @@ def test_get_category_books():
 
 @allure.feature("API")
 @allure.story("Получение списка книг по фильтрам")
+@pytest.mark.api_test
 @pytest.mark.positive_test
 def test_get_filtres_books():
         filter_params = {
@@ -80,6 +95,7 @@ def test_get_filtres_books():
 
 @allure.feature("API")
 @allure.story("Получение списка книг по автору")
+@pytest.mark.api_test
 @pytest.mark.positive_test
 def test_get_books_by_author():      
         author_params = {
@@ -92,6 +108,7 @@ def test_get_books_by_author():
 
 @allure.feature("API")
 @allure.story("Получение списка книг по наличию")
+@pytest.mark.api_test
 @pytest.mark.positive_test
 def test_get_available_books():
         available_filter_params = {
